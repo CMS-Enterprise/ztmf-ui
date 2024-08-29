973f82aec3b17a5250b1ec0aab0604e52528ad73
Tran, Alex <Alex.Tran@noblis.org>
2024-08-29 08:29:23 -0400

    removed axios body object

--- src/axiosConfig.ts
+++ src/axiosConfig.ts
@@ -1,17 +1,5 @@
 import axios, { AxiosInstance } from 'axios'
 
-const axiosInstance: AxiosInstance = axios.create({
-  baseURL: '/api/v1/',
-  headers: {
-    'Content-Type': 'application/json',
-  },
-  // withCredentials: true,
-})
-
-// Set the Authorization header conditionally based on the environment
-if (process.env.NODE_ENV === 'development') {
-  axiosInstance.defaults.headers.common['Authorization'] =
-    `Bearer ${import.meta.env.VITE_AUTH_TOKEN3 || ''}`
-}
+const axiosInstance: AxiosInstance = axios.create()
 
 export default axiosInstance
