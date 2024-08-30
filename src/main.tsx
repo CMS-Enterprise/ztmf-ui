2d67ae54364cc5b3d814c2a93a0ef4b651efa532
Tran, Alex <Alex.Tran@noblis.org>
2024-08-30 09:19:01 -0400

    Removing the # infront of login when redirected

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@
  */
 import * as React from 'react'
 import * as ReactDOMClient from 'react-dom/client'
-import { RouterProvider } from 'react-router-dom'
+import { RouterProvider, BrowserRouter } from 'react-router-dom'
 import CONFIG from '@/utils/config'
 import router from '@/router/router'
 import { SIGN_IN_GREETING } from '@/locales/en'
@@ -19,7 +19,9 @@ import onPerfEntry from './utils/onPerfEntry'
   // create the React root node and render the application
   ReactDOMClient.createRoot(rootElement).render(
     <React.StrictMode>
-      <RouterProvider router={router} />
+      <BrowserRouter>
+        <RouterProvider router={router} />
+      </BrowserRouter>
     </React.StrictMode>
   )
 
