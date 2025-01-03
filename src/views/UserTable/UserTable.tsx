6dc18d9490229c25b6179e71955015a0cfe567e2
Tran, Alex <Alex.Tran@noblis.org>
2025-01-03 11:42:50 -0500

    adding duration to userTable

--- src/views/UserTable/UserTable.tsx
+++ src/views/UserTable/UserTable.tsx
@@ -396,6 +396,7 @@ export default function UserTable() {
       <CustomSnackbar
         open={open}
         handleClose={handleCloseSnackbar}
+        duration={2000}
         severity="success"
         text="Saved"
       />
