d1a19dcdaff97e7f25968ce951c2d7825648d1e6
Tran, Alex <Alex.Tran@noblis.org>
2025-03-07 16:32:26 -0500

    adding css styling to breadcrumbs

--- src/components/BreadCrumbs/BreadCrumbs.tsx
+++ src/components/BreadCrumbs/BreadCrumbs.tsx
@@ -3,6 +3,7 @@ import { useLocation, Link as RouterLink } from 'react-router-dom'
 import Link, { LinkProps } from '@mui/material/Link'
 import { Typography, Box } from '@mui/material'
 import { capitalize } from 'lodash'
+import { red } from '@mui/material/colors'
 interface LinkRouterProps extends LinkProps {
   to: string
   replace?: boolean
@@ -15,15 +16,29 @@ export default function BreadCrumbs() {
   // let currentLink: string = ''
   const homeLink = [
     <LinkRouter underline="hover" to="/" key={'home'}>
-      <Typography>Dashboard</Typography>
+      <Typography
+        sx={{
+          ml: 2,
+          color: 'white',
+        }}
+      >
+        Dashboard
+      </Typography>
     </LinkRouter>,
   ]
-  const homeText = [<Typography key={'homeText'}>Dashboard</Typography>]
+  const homeText = [
+    <Typography key={'homeText'} sx={{ ml: 2, color: '#a6a6a6' }}>
+      Dashboard
+    </Typography>,
+  ]
   const crumbs = location.pathname.split('/').filter((x) => x)
   const path = crumbs.map((value) => {
     const text = value.replace('_', ' ')
     return (
-      <Typography sx={{ display: 'inline', whiteSpace: 'nowrap' }} key={value}>
+      <Typography
+        sx={{ display: 'inline', whiteSpace: 'nowrap', color: '#a6a6a6' }}
+        key={value}
+      >
         {capitalize(text)}
       </Typography>
     )
@@ -36,12 +51,16 @@ export default function BreadCrumbs() {
       <Breadcrumbs
         aria-label="breadcrumb"
         sx={{
-          display: 'flex',
-          flexWrap: 'nowrap',
-          flexDirection: 'row',
+          // display: 'flex',
+          // flexWrap: 'nowrap',
+          // flexDirection: 'row',
           '& .MuiBreadcrumbs-ol': {
             flexDirection: 'row',
           },
+          backgroundColor: '#0d2499',
+          borderRadius: 1,
+          // color: 'white',
+          mb: 1,
         }}
         separator="›"
       >
