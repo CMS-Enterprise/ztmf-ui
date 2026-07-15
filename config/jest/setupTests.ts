2066a6208927418a17d25949c70b101ffac567a5
Calvin Costa <calvin.costa@va.com>
2026-07-15 16:46:54 -0400

    add tests

--- config/jest/setupTests.ts
+++ config/jest/setupTests.ts
@@ -3,6 +3,14 @@ import 'jest-fetch-mock'
 import { TextEncoder, TextDecoder } from 'util'
 import { webcrypto } from 'crypto'
 
+// jsdom does not implement ResizeObserver; stub it for components that use it
+// (e.g. Recharts ResponsiveContainer).
+global.ResizeObserver = class {
+  observe() {}
+  unobserve() {}
+  disconnect() {}
+} as unknown as typeof ResizeObserver
+
 // Polyfill for React Router 7 which requires TextEncoder/TextDecoder
 global.TextEncoder = TextEncoder
 global.TextDecoder = TextDecoder as typeof global.TextDecoder
